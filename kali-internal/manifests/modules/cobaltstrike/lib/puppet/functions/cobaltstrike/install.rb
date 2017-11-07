07b9c27fd657abd6c77c1b676d803e7fad13fb41
James Cook <goduke44@gmail.com>
2017-11-07 01:46:19 -0700

    remove un-needed functions

--- kali-internal/manifests/modules/cobaltstrike/lib/puppet/functions/cobaltstrike/install.rb
+++
@@ -1,9 +0,0 @@
-Puppet::Functions.create_function(:'cobaltstrike::install') do
-  dispatch :install do
-    param 'String', :key
-  end
-
-  def install(key)
-    `printf "\n" | /opt/cobaltstrike/./update`
-  end
-end
