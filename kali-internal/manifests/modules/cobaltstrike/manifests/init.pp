bb361ebf8d5f025c06a788f50d13d1f67fe5797b
James Cook <goduke44@gmail.com>
2017-11-07 16:04:41 -0700

    Cobalt Strike tests
    Only perform the exec onlyif the file exists

--- kali-internal/manifests/modules/cobaltstrike/manifests/init.pp
+++ kali-internal/manifests/modules/cobaltstrike/manifests/init.pp
@@ -5,16 +5,19 @@ class cobaltStrike (String $cs_key) {
     command => 'tar zxf /tmp/cobaltstrike.tgz -C /opt',
     creates => '/opt/cobaltstrike',
     path => '/bin/',
+    onlyif => '/usr/bin/test -f /tmp/cobaltstrike.tgz',
   }
 
   exec { 'cobaltstrike install':
     command => "printf \"${cs_key}\r\" | java -XX:ParallelGCThreads=4 -XX:+AggressiveHeap -XX:+UseParallelGC -jar /opt/cobaltstrike/update.jar",
     path => '/usr/bin/',
+    onlyif => '/usr/bin/test -f /opt/cobaltstrike/update.jar',
   }
 
   exec { 'update':
     command => '/opt/cobaltstrike/update',
     cwd => '/opt/cobaltstrike',
+    onlyif => '/usr/bin/test -f /opt/cobaltstrike/update',
   }
 
 }
